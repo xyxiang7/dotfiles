a26579c88b1d37ec5b906b0efddbb26cfa49b5a0
xyxiang7 <xyxiang7@gmail.com>
2023-07-02 15:16:05 -0400

    upd kernel version

--- sys/linux-version.sh
+++ sys/linux-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
-sudo bash ubuntu-mainline-kernel.sh -i 5.8.0
+sudo bash ubuntu-mainline-kernel.sh -i 6.1.0
 sudo reboot -h now
